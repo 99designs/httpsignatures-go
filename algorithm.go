eba1f444cefd3958c77e10ac7491331d9bb2e42a
Eric J. Holmes <eric@ejholmes.net>
2017-10-24 20:52:46 -0700

    Implement rsa-sha1 and rsa-sha256 algorithms

--- algorithm.go
+++ algorithm.go
@@ -1,22 +1,33 @@
 package httpsignatures
 
 import (
-	"crypto/sha1"
-	"crypto/sha256"
+	"crypto"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/rsa"
 	"errors"
-	"hash"
 )
 
 var (
-	AlgorithmHmacSha256 = &Algorithm{"hmac-sha256", sha256.New}
-	AlgorithmHmacSha1   = &Algorithm{"hmac-sha1", sha1.New}
+	AlgorithmHmacSha256 = &Algorithm{"hmac-sha256", hmacSign(crypto.SHA256), hmacVerify(crypto.SHA256)}
+	AlgorithmHmacSha1   = &Algorithm{"hmac-sha1", hmacSign(crypto.SHA1), hmacVerify(crypto.SHA1)}
+	AlgorithmRsaSha256  = &Algorithm{"rsa-sha256", rsaSign(crypto.SHA256), rsaVerify(crypto.SHA256)}
+	AlgorithmRsaSha1    = &Algorithm{"rsa-sha1", rsaSign(crypto.SHA1), rsaVerify(crypto.SHA1)}
 
 	ErrorUnknownAlgorithm = errors.New("Unknown Algorithm")
 )
 
+// signFn signs message m using key k.
+type signFn func(k interface{}, m []byte) ([]byte, error)
+
+// verifyFn verifies that signature s, for message m was signed by key k.
+type verifyFn func(k interface{}, m []byte, s []byte) bool
+
 type Algorithm struct {
 	name string
-	hash func() hash.Hash
+
+	sign   signFn
+	verify verifyFn
 }
 
 func algorithmFromString(name string) (*Algorithm, error) {
@@ -25,7 +36,57 @@ func algorithmFromString(name string) (*Algorithm, error) {
 		return AlgorithmHmacSha1, nil
 	case AlgorithmHmacSha256.name:
 		return AlgorithmHmacSha256, nil
+	case AlgorithmRsaSha1.name:
+		return AlgorithmRsaSha1, nil
+	case AlgorithmRsaSha256.name:
+		return AlgorithmRsaSha256, nil
 	}
 
 	return nil, ErrorUnknownAlgorithm
+}
+
+// hmacSign returns a function that will HMAC sign some message using the given
+// hash function.
+func hmacSign(h crypto.Hash) signFn {
+	return func(k interface{}, m []byte) ([]byte, error) {
+		hash := hmac.New(h.New, []byte(k.(string)))
+		hash.Write(m)
+		return hash.Sum(nil), nil
+	}
+}
+
+// hmacVerify returns a function that will verify that the signature signed with
+// the given hashfn matches the calculated signature.
+func hmacVerify(h crypto.Hash) verifyFn {
+	sign := hmacSign(h)
+	return func(k interface{}, m []byte, s []byte) bool {
+		calculatedSignature, err := sign(k, m)
+		if err != nil {
+			return false
+		}
+
+		return hmac.Equal(calculatedSignature, s)
+	}
+}
+
+// rsaSign returns a function that will sign a message with an RSA private key,
+// using the given hash function.
+func rsaSign(h crypto.Hash) signFn {
+	return func(k interface{}, m []byte) ([]byte, error) {
+		hash := h.New()
+		hash.Write(m)
+		hashed := hash.Sum(nil)
+		return rsa.SignPKCS1v15(rand.Reader, k.(*rsa.PrivateKey), h, hashed[:])
+	}
+}
+
+// rsaVerify returns a function that will verify that a message was signed with
+// an RSA private key.
+func rsaVerify(h crypto.Hash) verifyFn {
+	return func(k interface{}, m []byte, s []byte) bool {
+		hash := h.New()
+		hash.Write(m)
+		hashed := hash.Sum(nil)
+		return rsa.VerifyPKCS1v15(k.(*rsa.PublicKey), h, hashed[:], s) == nil
+	}
 }
--- examples_test.go
+++ examples_test.go
@@ -1,9 +1,36 @@
 package httpsignatures_test
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"net/http"
 
 	"github.com/99designs/httpsignatures-go"
+)
+
+const (
+	ExamplePrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDCFENGw33yGihy92pDjZQhl0C36rPJj+CvfSC8+q28hxA161QF
+NUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6Z4UMR7EOcpfdUE9Hf3m/hs+F
+UR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJwoYi+1hqp1fIekaxsyQIDAQAB
+AoGBAJR8ZkCUvx5kzv+utdl7T5MnordT1TvoXXJGXK7ZZ+UuvMNUCdN2QPc4sBiA
+QWvLw1cSKt5DsKZ8UETpYPy8pPYnnDEz2dDYiaew9+xEpubyeW2oH4Zx71wqBtOK
+kqwrXa/pzdpiucRRjk6vE6YY7EBBs/g7uanVpGibOVAEsqH1AkEA7DkjVH28WDUg
+f1nqvfn2Kj6CT7nIcE3jGJsZZ7zlZmBmHFDONMLUrXR/Zm3pR5m0tCmBqa5RK95u
+412jt1dPIwJBANJT3v8pnkth48bQo/fKel6uEYyboRtA5/uHuHkZ6FQF7OUkGogc
+mSJluOdc5t6hI1VsLn0QZEjQZMEOWr+wKSMCQQCC4kXJEsHAve77oP6HtG/IiEn7
+kpyUXRNvFsDE0czpJJBvL/aRFUJxuRK91jhjC68sA7NsKMGg5OXb5I5Jj36xAkEA
+gIT7aFOYBFwGgQAQkWNKLvySgKbAZRTeLBacpHMuQdl1DfdntvAyqpAZ0lY0RKmW
+G6aFKaqQfOXKCyWoUiVknQJAXrlgySFci/2ueKlIE1QqIiLSZ8V8OlpFLRnb1pzI
+7U1yQXnTAEFYM560yJlzUpOb1V4cScGd365tiSMvxLOvTA==
+-----END RSA PRIVATE KEY-----`
+	ExamplePublicyKey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCFENGw33yGihy92pDjZQhl0C3
+6rPJj+CvfSC8+q28hxA161QFNUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6
+Z4UMR7EOcpfdUE9Hf3m/hs+FUR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJw
+oYi+1hqp1fIekaxsyQIDAQAB
+-----END PUBLIC KEY-----`
 )
 
 func Example_signing() {
@@ -13,6 +40,20 @@ func Example_signing() {
 	httpsignatures.DefaultSha256Signer.SignRequest("KeyId", "Key", r)
 	// OR Sign using the 'Authorization' header
 	httpsignatures.DefaultSha256Signer.AuthRequest("KeyId", "Key", r)
+
+	http.DefaultClient.Do(r)
+}
+
+func Example_signingRSA() {
+	block, _ := pem.Decode([]byte(ExamplePrivateKey))
+	privateKey, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
+
+	r, _ := http.NewRequest("GET", "http://example.com/some-api", nil)
+
+	// Sign using the 'Signature' header
+	httpsignatures.DefaultRsaSha256Signer.SignRequestRSA("KeyId", privateKey, r)
+	// OR Sign using the 'Authorization' header
+	httpsignatures.DefaultRsaSha256Signer.AuthRequestRSA("KeyId", privateKey, r)
 
 	http.DefaultClient.Do(r)
 }
@@ -44,6 +85,31 @@ func Example_verification() {
 
 		var key string // = lookup using sig.KeyID
 		if !sig.IsValid(key, r) {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		// request was signed correctly.
+	}
+}
+
+func Example_verificationRSA() {
+	_ = func(w http.ResponseWriter, r *http.Request) {
+		sig, err := httpsignatures.FromRequest(r)
+		if err != nil {
+			// Probably a malformed header
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			panic(err)
+		}
+
+		// if you have headers that must be signed check
+		// that they are in sig.Headers
+
+		var pemPublicKeyBytes []byte // = lookup using sig.KeyID
+		block, _ := pem.Decode(pemPublicKeyBytes)
+		publicKey, _ := x509.ParsePKIXPublicKey(block.Bytes)
+
+		if !sig.IsValidRSA(publicKey.(*rsa.PublicKey), r) {
 			http.Error(w, "Forbidden", http.StatusForbidden)
 			return
 		}
